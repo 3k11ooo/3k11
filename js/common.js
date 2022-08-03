689f997570a985b8322bf773ced35465acd06399
SmokyRat <tkiput467@gmail.com>
2022-08-04 01:15:25 +0900

    大枠完成

---
+++ js/common.js
@@ -0,0 +1,25 @@
+function header(rootDir) {
+    $.ajax({
+        url: rootDir + "subContents/header.html", // htmlファイル読み込み
+        cache: false, //キャッシュ
+        async: false, //非同期
+        dataType: 'html',
+        success: function(html){
+
+            html = html.replace(/\{\$root\}/g, rootDir);
+            document.write(html);
+        }
+    });
+}
+
+function footer(){
+    $.ajax({
+        url: "subContents/footer.html",
+        cache: false,
+        async: false, 
+        dataType: 'html',
+        success: function(html){
+            document.write(html);
+        }
+    })
+}
